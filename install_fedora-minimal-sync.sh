bc985f55fa2696e04a6f8afb8e4e0eb0dd1c3e09
IceBear <ib-github@myrl.net>
2018-09-14 23:09:32 +0200

    Add OpenSSH server config

--- install_fedora-minimal-sync.sh
+++ install_fedora-minimal-sync.sh
@@ -12,3 +12,11 @@ sudo dnf -y clean packages
 # https://www.synology.com/en-global/support/download/VirtualDSM
 sudo dnf -y localinstall https://global.download.synology.com/download/Tools/CloudStationDrive/${synology_cloud_station_drive_version}/Fedora/Installer/x86_64/synology-cloud-station-drive-${synology_cloud_station_drive_version}.x86_64.rpm
 
+# enable services
+sudo systemctl enable sshd.service
+
+# settings
+sudo sed -i '/^PermitRootLogin/c\PermitRootLogin no' /etc/ssh/sshd_config
+sudo sed -i '/^#PubkeyAuthentication/c\PubkeyAuthentication yes' /etc/ssh/sshd_config
+sudo sed -i '/^PasswordAuthentication/c\PasswordAuthentication no' /etc/ssh/sshd_config
+
